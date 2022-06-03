41677d0e8ae128dd70a2e8359024dd56601d7694
ramadhani25 <ramadhaniak1125@gmail.com>
2022-06-03 10:10:08 +0700

    add jsconfig

--- src/components/RouteComponent/index.jsx
+++ src/components/RouteComponent/index.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-
-import Login from '../../pages/Login'
-import Dashboard from '../../pages/Dashboard'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard, Login } from "pages";
 
 export default function RouteComponent() {
   return (
     <Routes>
-        <Route path='/'>
-            <Route index element={<Dashboard/>}/>
-            <Route path='login' element={<Login/>}/>
-        </Route>
+      <Route path="/">
+        <Route index element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+      </Route>
     </Routes>
-  )
+  );
 }
