dc8e9cf8f100c09da4d5e84eb6605c2cf04393d5
ramadhani25 <ramadhaniak1125@gmail.com>
2022-06-06 23:05:05 +0700

    slicing login page

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,44 +1,39 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-import { Button, TextField, Typography } from '@mui/material'
+import { Button, TextField, Typography } from "@mui/material";
 
-import { CustomInput } from 'components'
+import { CustomInput } from "components";
 
 export default function Dashboard() {
+  const [input, setInput] = useState("");
 
-  const [input,setInput] = useState('')
-
-  console.log(input)
+  console.log(input);
 
   return (
     <>
-    {/* example material ui*/}
-    <Button variant='contained'>default</Button>
-    
-    <Typography variant='body2'>Halo</Typography>
-    <Typography variant='body3'>Halo</Typography>
-    
-    <TextField
-    label='halo@gmail.com'
-    placeholder='hlo'
-    />
-    
-    <CustomInput 
-    label='Email' 
-    isError={true}
-    onChange={(e)=>setInput(e)}
-    value={input}
-    type='date'
-    errorMessage='Email is Not Valid'
-    />
+      {/* example material ui*/}
+      <Button variant="contained">default</Button>
 
-    <CustomInput 
-    label='Password' 
-    onChange={(e)=>setInput(e)}
-    value={input}
-    type='password'
-    />
+      <Typography variant="body2">Halo</Typography>
+      <Typography variant="body3">Halo</Typography>
 
+      <TextField label="halo@gmail.com" placeholder="hlo" />
+
+      <CustomInput
+        label="Email"
+        isError={true}
+        onChange={(e) => setInput(e)}
+        value={input}
+        type="date"
+        errorMessage="Email is Not Valid"
+      />
+
+      <CustomInput
+        label="Password"
+        onChange={(e) => setInput(e)}
+        value={input}
+        type="password"
+      />
     </>
-  )
+  );
 }
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+// components
+import { Box } from "@mui/system";
+import {
+  Button,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+// assets
+import Logo from "assets/svg/Logo2.svg";
 
 export default function Login() {
-  return <div>Login Baru</div>;
+  const [values, setValues] = useState({
+    showPassword: false,
+  });
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: "100vh",
+        bgcolor: "primary.main",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: "23rem",
+          height: "30rem",
+          bgcolor: "white",
+          margin: "1.2rem",
+          borderRadius: "24px",
+          boxShadow: "0px 4px 4px 0px rgba(0,0,0,0.25)",
+          padding: "2rem 2.5rem",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img width="128px" src={Logo} alt="logo" />
+          <Box
+            sx={{
+              fontSize: "2rem",
+              fontWeight: "bold",
+              color: "primary.main",
+              pt: "0.5rem",
+              pb: "2rem",
+            }}
+          >
+            Care Hospital
+          </Box>
+        </Box>
+
+        <Box component="form">
+          <InputLabel
+            shrink
+            htmlFor="login-email"
+            sx={{ fontSize: "18px", color: "black" }}
+          >
+            Email
+          </InputLabel>
+          <TextField fullWidth id="login-email" size="small" type="text" />
+          <br />
+          <br />
+          <InputLabel
+            shrink
+            htmlFor="login-password"
+            sx={{ fontSize: "18px", color: "black" }}
+          >
+            Password
+          </InputLabel>
+          <OutlinedInput
+            fullWidth
+            id="login-password"
+            size="small"
+            type={values.showPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+          <br />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "end",
+              fontSize: "14px",
+              paddingTop: "0.3rem",
+            }}
+          >
+            <Link style={{ textDecoration: "none", color: "#B3BDC9" }} to="/">
+              Forget password?
+            </Link>
+          </Box>
+          <br />
+          <Button fullWidth variant="contained">
+            Sign in
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
