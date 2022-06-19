5829ea75819ea6d78730e6235ae268f1df738f3a
ramadhani25 <ramadhaniak1125@gmail.com>
2022-06-19 21:04:44 +0700

    merge to login-page branch

--- src/pages/Dashboard/components/WelcomeBox.jsx
+++ src/pages/Dashboard/components/WelcomeBox.jsx
@@ -5,60 +5,53 @@ import { Box, Typography, Divider } from '@mui/material'
 import BgWelcome from 'assets/image/bg_welcome.jpg'
 
 export default function WelcomeBox() {
+  
+  let moment = require('moment')
+
   return (
-
     <Box
-    sx={{
-        backgroundImage : `url(${BgWelcome})`,
-        backgroundSize : 'cover',
-        backgroundRepeat : 'no-repeat',
-        borderRadius : '8px'
-    }}
+      sx={{
+        backgroundImage: `url(${BgWelcome})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        borderRadius: '8px',
+      }}
     >
-
-        <Box
+      <Box
         sx={{
-        display : 'flex',
-        padding : '75px',
-        background : 'linear-gradient(89.89deg, #4E89A8 6.63%, #00000000 105.65%)',
-        borderRadius : '8px',
-        boxShadow : '0px 4px 4px rgba(0, 0, 0, 0.25)'
+          display: 'flex',
+          padding: '75px',
+          background:
+            'linear-gradient(89.89deg, #4E89A8 6.63%, #00000000 105.65%)',
+          borderRadius: '8px',
+          boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
         }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            width: '50%',
+            color: 'white',
+            flexDirection: 'column',
+            rowGap: '10px',
+            justifyContent: 'center',
+          }}
         >
-        
-            <Box
+          <Typography variant='body1'>Welcome</Typography>
+
+          <Typography variant='h3'>Hai, Kevin</Typography>
+
+          <Divider
             sx={{
-                display : 'flex',
-                width : '50%',
-                color : 'white',
-                flexDirection : 'column',
-                rowGap : '10px',
-                justifyContent : 'center'
+              borderColor: 'white',
             }}
-            >
-            
-                <Typography variant='body1'>
-                Welcome
-                </Typography>
+          />
 
-                <Typography variant='h3'>
-                Hai, Kevin
-                </Typography>
+          <Typography variant='body1'>Stay safe and wash your hand</Typography>
 
-                <Divider
-                sx={{
-                borderColor:'white'
-                }}
-                />
-
-                <Typography variant='body1'>
-                Stay safe and wash your hand
-                </Typography>
-            
-            </Box>
-
+          <Typography variant='body1'>{moment().format('HH[:]mm')}</Typography>
         </Box>
-    
+      </Box>
     </Box>
   )
 }
