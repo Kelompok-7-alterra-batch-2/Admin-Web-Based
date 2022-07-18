41b5d5c1460c46abe3ca8ecfb5f6f04f824f585d
Afwan-Pratama <phil.pratama@gmail.com>
2022-07-18 16:51:28 +0700

    add : history page

--- src/constants/history.js
+++ src/constants/history.js
@@ -10,6 +10,11 @@ export const dataHead = [
     fieldChild: 'name',
   },
   {
+    headerName: 'Department',
+    fieldname: 'department',
+    fieldChild: 'name',
+  },
+  {
     headerName: 'Doctor',
     fieldname: 'doctor',
     fieldChild: 'name',
@@ -19,4 +24,60 @@ export const dataHead = [
     fieldname: 'outpatientCondition',
     fieldChild: 'conditions',
   },
+  {
+    headerName: 'Edit',
+    fieldname: 'edit',
+    delete: true,
+  },
 ]
+
+export const field = [
+  {
+    title: 'Patient',
+    fieldname: 'patient_id',
+    type: 'search',
+    endPoint: 'patients',
+    value: 'patient',
+    valueChild: 'id',
+    param: {
+      first: {
+        title: 'name',
+        value: 'id',
+      },
+      second: 'address',
+    },
+  },
+  {
+    title: 'Appointment Date',
+    fieldname: 'date',
+    type: 'date',
+  },
+  {
+    title: 'Appointment Time',
+    fieldname: 'arrivalTime',
+    type: 'arrival-time',
+  },
+  {
+    title: 'Department',
+    fieldname: 'department_id',
+    type: 'department',
+    value: 'department',
+    valueChild: 'id',
+  },
+  {
+    title: 'Doctor',
+    fieldname: 'doctor_id',
+    type: 'doctor',
+    value: 'doctor',
+    valueChild: 'id',
+    disabled: false,
+  },
+  {
+    title: 'Appointment Reason',
+    fieldname: 'appointmentReason',
+    type: 'area',
+    rows: 3,
+  },
+]
+
+
